c23fab565c8f0319c0aab4c918b80223f3e41ef9
igalfsg <igal@Macs-MacBook-Pro-99.local>
2016-07-18 20:09:20 -0400

    finish fitcalc calculations

--- iOS/Fit_Life/ParseStarterProject/FitnessCalViewController.swift
+++ iOS/Fit_Life/ParseStarterProject/FitnessCalViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import DLRadioButton
 
+
 class FitnessCalViewController: UIViewController {
 
     @IBOutlet weak var rep_btn: UIButton!
@@ -20,10 +21,10 @@ class FitnessCalViewController: UIViewController {
     @IBOutlet weak var cm: DLRadioButton!
     @IBOutlet weak var lb: DLRadioButton!
     @IBOutlet weak var inch: DLRadioButton!
+    @IBOutlet weak var result_txt: UITextView!
     
-    
-    var bool_weight = 0
-    var bool_height = 0
+    var weightbmi = 0.0;
+    var heightbmi = 0.0;
     var type = 1
     
     override func viewDidLoad() {
@@ -41,24 +42,23 @@ class FitnessCalViewController: UIViewController {
     @objc @IBAction private func kgorlb(radioButton : DLRadioButton) {
         
         if radioButton.selectedButton()!.titleLabel!.text! == "kg"{
-            bool_weight = 0
+            weightbmi = 1.0
         }
         else{
-            bool_weight = 1
+            weightbmi = 0.453592
         }
-       print(bool_weight)
     }
     
     //height
     @objc @IBAction private func height_info(radioButton : DLRadioButton) {
         
         if radioButton.selectedButton()!.titleLabel!.text! == "cm"{
-            bool_height = 0
+            heightbmi = 1.0
         }
         else{
-            bool_height = 1
+            heightbmi = 2.54
         }
-        print(bool_height)
+        
     }
 
     @IBAction func hr_btn_press(sender: AnyObject) {
@@ -66,6 +66,12 @@ class FitnessCalViewController: UIViewController {
         lb.hidden = true
         cm.hidden = true
         inch.hidden = true
+        top_txt.placeholder = "Age"
+        bottom_txt.placeholder = "Resting Heart Rate"
+        print("hr")
+        result_txt.text = ""
+        top_txt.text = ""
+        bottom_txt.text = ""
         type = 2
     }
     
@@ -74,7 +80,13 @@ class FitnessCalViewController: UIViewController {
         lb.hidden = false
         cm.hidden = false
         inch.hidden = false
+        top_txt.placeholder = "Weight"
+        bottom_txt.placeholder = "Height"
         type = 1
+        result_txt.text = ""
+        top_txt.text = ""
+        bottom_txt.text = ""
+        print("Yoooooo")
     }
 
     @IBAction func rep_pressed(sender: AnyObject) {
@@ -82,9 +94,78 @@ class FitnessCalViewController: UIViewController {
         lb.hidden = true
         cm.hidden = true
         inch.hidden = true
+        top_txt.placeholder = "Repetitions"
+        bottom_txt.placeholder = "Weight"
+        print("rep")
+        result_txt.text = ""
+        top_txt.text = ""
+        bottom_txt.text = ""
         type = 0
     }
     
+
+    @IBAction func calculate_magic(sender: AnyObject) {
+        if top_txt.text == "" || bottom_txt == ""{
+            if type == 0{//1 rep max
+                result_txt.text = " Please enter the number of repetitions and weight used"
+            }
+            else if type == 1{//bmi
+                result_txt.text = " Please enter your height and weight and select the correct units"
+            }
+            else{//hr
+                result_txt.text = " Please enter your resting heartrate and your age"
+            }
+        }
+        else if type == 0{//1 rep max
+            let weight = Double(bottom_txt.text!)
+            let reps = Double(top_txt.text!)
+            let max = weight! / (1.0278 - (0.0278 * reps!))
+            print((0.0278 * reps!))
+            let six = Double(round((max * 0.83) * 100.0) / 100.0)
+            let eight = Double(round((max * 0.78) * 100.0) / 100.0)
+            let ten = Double(round((max * 0.75) * 100.0) / 100.0)
+            let twelve = Double(round((max * 0.70) * 100.0) / 100.0)
+            let fifteen = Double(round((max * 0.67) * 100.0) / 100.0)
+            
+            result_txt.text = "1 Rep:  " + String(Double(round(max * 100.0) / 100.0)) +
+            "\n 6 Reps: " + String(six) +
+            "\n 8 Reps: " + String(eight) +
+            "\n 10 Reps:" + String(ten) +
+            "\n 12 Reps:" + String(twelve) +
+            "\n 15 Reps:" + String(fifteen)
+        }
+        else if type == 1{//bmi
+            let weight = Double(bottom_txt.text!)! * weightbmi
+            let height = Double(top_txt.text!)! * heightbmi
+            let result = Double(round(weight / ((height / 100.0) * (height / 100.0)) * 100.0) / 100.0)
+            if result < 18.5 {
+                result_txt.text = String(result) + " Underweight"
+            }
+            else if result < 25{
+                result_txt.text = String(result) + " Normal weight"
+            }
+            else if result < 30{
+                result_txt.text = String(result) + " Overweight"
+            }
+            else{
+                result_txt.text = String(result) + " Obese"
+            }
+        }
+        else{//hr
+            let resthr = Int(bottom_txt.text!)
+            let age = Int(top_txt.text!)
+            let zone1 = Int(0.59 * Double(220 - age! - resthr!)) + resthr!
+            let zone2_min = Int(0.60 * Double(220 - age! - resthr!)) + resthr!
+            let zone2_max = Int(0.78 * Double(220 - age! - resthr!)) + resthr!
+            let zone3_min = Int(0.79 * Double(220 - age! - resthr!)) + resthr!
+            let zone3_max = Int(0.90 * Double(220 - age! - resthr!)) + resthr!
+            
+            result_txt.text = "Health \t\t\t up to: " + String(zone1) + " bpm\n" +
+            "Weight Loss: \t\t from: " + String(zone2_min) + " to " + String(zone2_max) + " bpm\n" +
+            "Cardio improvement: \t\t from: " + String(zone3_min) + " to " + String(zone3_max) + " bpm\n"
+        }
+
+    }
     
     /*
     // MARK: - Navigation
