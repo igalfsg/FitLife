1a9962f3e6f25fa58cdfe78a77815a6a2ce7c0f2
Jhon Villalobos <villajohn@yahoo.com>
2016-07-23 03:03:56 -0400

    Update for all screens

--- iOS/Fit_Life/ParseStarterProject/RegisterViewController.swift
+++ iOS/Fit_Life/ParseStarterProject/RegisterViewController.swift
@@ -19,6 +19,8 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var WeightTextField: UITextField!
     @IBOutlet weak var kgBtn: DLRadioButton!
     @IBOutlet weak var interest_button: DLRadioButton!
+    @IBOutlet weak var interest_view: UIStackView!
+    
     var sex = ""
     var peso = ""
     var interests = ""
@@ -33,8 +35,6 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         EmailTextField.delegate = self
         PasswordTextField.delegate = self
         BirthdateTextField.delegate = self
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
